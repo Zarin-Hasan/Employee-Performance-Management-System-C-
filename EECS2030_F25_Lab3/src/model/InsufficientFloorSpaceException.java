3cea42c396b6596c9a34c8ab7bfa41a8091c557e
Zarin-Hasan <zarin989@my.yorku.ca>
2025-11-11 15:07:08 -0500

    Delete EECS2030_F25_Lab3 directory

--- EECS2030_F25_Lab3/src/model/InsufficientFloorSpaceException.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-@SuppressWarnings("serial")
-public class InsufficientFloorSpaceException extends Exception {
-	public InsufficientFloorSpaceException(String s) {
-		super(s);
-	}
-}
